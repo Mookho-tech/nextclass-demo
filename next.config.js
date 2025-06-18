1e8ce35bb4aae605a246b7083c58caa4b59505f7
Mookho <boitumelomercy@email.com>
2025-06-19 01:15:10 +0200

    Add GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ module.exports = {
   images: {
     domains: ['lh3.googleusercontent.com'], // Add any image domains you need
   },
+  
 };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Add this line for static export
+  images: {
+    unoptimized: true, // Required for static export
+  },
+}
+
+module.exports = nextConfig
